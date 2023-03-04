226bd5f1319d221e565f418685efeb0d5413ae2a
LucasJanak <ljanak262@mycr.redwoods.edu>
2023-03-03 18:22:07 -0800

    Version 1.1

--- src/Lab006.java
+++ src/Lab006.java
@@ -1,15 +1,35 @@
+import java.util.Scanner;
 public class Lab006 {
-    public int m;
-    public int n;
-
-    public boolean Lab006(int m,int n){
+    int m;
+    int n;
+    public Lab006(int n,int m){
         this.m = m;
         this.n = n;
-        if (n % m == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        /**
+         * Assigns local variables from main as instance variables to be used by isDivisible
+         *
+         * @param Gathered from user input, n ultimately as the numerator and m as denominator
+         */
         }
 
+        public boolean isDivisible(int n, int m){
+        return n % m == 0;
+            /**
+             * Tests whether n is perfectly divisible by m
+             *
+             * @param n is the numerator, m is the denominator
+             * @return true if it is divisible, false if not
+             */
+    }
+        public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("What is the number being divided?");
+         int n = in.nextInt();
+        System.out.println("What number are you checking if it is divisible by?");
+         int m = in.nextInt();
+         Lab006 myObject = new Lab006(n, m);
+         System.out.println("Is it divisible? "+myObject.isDivisible(n,m));
+
+
+    }
 }
